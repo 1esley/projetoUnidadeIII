579a9cc2b02f1eb811406480e9ef04b028115a70
1esley <159152636+1esley@users.noreply.github.com>
2024-06-12 15:08:35 -0300

    Delete PROJETO UNIDADE III directory

--- PROJETO UNIDADE III/functCleintes.py
+++
@@ -1,7 +0,0 @@
-def lancheCarrinho(precoLanche, lanche, email, carrinhoCliente, carrinhos):
-    carrinhoCliente.append(precoLanche) # [2]
-    carrinhoCliente.append(lanche) # [3]
-    carrinhos[email] = carrinhoCliente
-    print('Combo adicionado ao carrinho')
-    print('Obrigado! Vá ao carrinho para confirmar a compra!')
-
